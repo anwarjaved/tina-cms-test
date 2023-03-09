44058675c27e7f106d9d8d346b25314827e8631c
Anwar <anwar@skybin.io>
2023-03-09 15:32:33 +0530

    Import

---
+++ .tina/config.ts
@@ -0,0 +1,76 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+	branch,
+	clientId: "b537e4d3-1ce1-44a1-9122-484cdcce614d", // Get this from tina.io
+	token: "8d65b2eadb08a0904a3dbe2d52f9405ef367d8c2", // Get this from tina.io
+	build: {
+		outputFolder: "admin",
+		publicFolder: "/",
+	},
+	media: {
+		tina: {
+			mediaRoot: "uploads",
+			publicFolder: "assets"
+		},
+	},
+	schema: {
+		collections: [
+			{
+				name: "post",
+				label: "Posts",
+				path: "_posts",
+				ui: {
+					filename: {
+						readonly: false,
+						slugify: values => {
+							const date = new Date();
+							const day = date.getDate();
+							const month = date.getMonth() + 1;
+							const year = date.getFullYear();
+
+							let currentDate = `${year}-${month}-${day}`;
+
+							return `${currentDate}-${values?.title?.toLowerCase().replace(/ /g, '-')}`
+						}
+					}
+				},
+				fields: [
+					{
+						type: "string",
+						name: "layout",
+						label: "Layout",
+						required: true,
+					},
+					{
+						type: "string",
+						name: "title",
+						label: "Title",
+						isTitle: true,
+						required: true,
+					},
+					{
+						type: "datetime",
+						name: "date",
+						label: "Date",
+						required: true,
+					},
+					{
+						type: "string",
+						name: "categories",
+						label: "Categories",
+					},
+					{
+						type: "rich-text",
+						name: "body",
+						label: "Body",
+						isBody: true,
+					},
+				],
+			},
+		],
+	},
+});
